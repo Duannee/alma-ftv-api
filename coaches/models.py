c5c9378aec5c04c7f7faa6f240bbf97f16714492
Duanne Moraes <duanne.souza@gmail.com>
2024-11-25 16:17:24 -0300

    feat: Create coaches model

--- coaches/models.py
+++ coaches/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Coach(models.Model):
+    GENRE_CHOICE = [("FEMALE", "FEMALE"), ("MALE", "MALE"), ("OTHERS", "OTHERS")]
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.TextField(max_length=20)
+    genre = models.CharField(max_length=7, choices=GENRE_CHOICE)
+
+    def __str__(self) -> str:
+        return self.nome
