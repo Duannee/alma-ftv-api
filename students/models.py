1a3be52b63a5d7650281f9af8422b53bd2c197b5
Duanne Moraes <duanne.souza@gmail.com>
2024-11-25 15:59:39 -0300

    fix: Recreate student app and model

--- students/models.py
+++ students/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Student(models.Model):
@@ -8,11 +7,14 @@ class Student(models.Model):
         ("INTERMEDIARY", "INTERMEDIARY"),
         ("ADVANCED", "ADVANCED"),
     ]
+    GENRE_CHOICE = [("FEMALE", "FEMALE"), ("MALE", "MALE"), ("OTHERS", "OTHERS")]
+
     name = models.CharField(max_length=255)
-    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(99)])
+    birth_date = models.DateField()
     email = models.EmailField()
-    phone = models.TextField(max_length=20)
+    phone = models.CharField(max_length=20)
+    genre = models.CharField(max_length=7, choices=GENRE_CHOICE)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICE)
 
     def __str__(self) -> str:
-        return self.nome
+        return self.name
