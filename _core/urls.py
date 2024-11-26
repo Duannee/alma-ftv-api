c48bab8b94acbe5c40f0ae2d889c604e62b491cb
Duanne Moraes <duanne.souza@gmail.com>
2024-11-26 15:57:15 -0300

    feat: Put logins urls in core urls

--- _core/urls.py
+++ _core/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("students.urls")),
-    path("api/", include("coaches.urls")),
+    path("api/", include("payments.urls")),
+    path("api/", include("logins.urls")),
 ]
