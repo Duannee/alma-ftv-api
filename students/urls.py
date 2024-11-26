61b30abb4e51bea1ab7b06b1fc27758c3b56af9e
Duanne Moraes <duanne.souza@gmail.com>
2024-11-26 16:04:06 -0300

    refactor: Change url student name

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import StudentCreateView
 
 urlpatterns = [
-    path("create/students/", StudentCreateView.as_view(), name="students"),
+    path("create/students/", StudentCreateView.as_view(), name="create-students"),
 ]
