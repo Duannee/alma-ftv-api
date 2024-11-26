1b6669d79b727e9d23e76a7737d1c2450fa73baf
Duanne Moraes <duanne.souza@gmail.com>
2024-11-26 15:36:49 -0300

    feat: Create login view

--- logins/views.py
+++ logins/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from users.models import User
+from .serializers import LoginUserSerializer
 
-# Create your views here.
+
+class LoginView(CreateAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not email or not password:
+            raise AuthenticationFailed("Email and password are required.")
+
+        user = authenticate(request, username=email, password=password)
+        if not user:
+            raise AuthenticationFailed("Invalid credentials.")
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "email": user.email,
+                "token": str(refresh.access_token),
+            }
+        )
