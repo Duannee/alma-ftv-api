dea36ab6c6ea2c56e7d8405a23664c31c1f6afe3
Duanne Moraes <duanne.souza@gmail.com>
2024-11-26 15:11:49 -0300

    feat: Create payment create view

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.forms import ValidationError
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import CreateAPIView
+from .models import Payment
+from .serializers import PaymentSerializer
+from students.models import Student
 
-# Create your views here.
+
+class CreatePaymentView(CreateAPIView):
+    serializer_class = PaymentSerializer
+
+    def perform_create(self, serializer):
+        student_payment = get_object_or_404(Student, pk=self.kwargs.get("student_id"))
+
+        if Payment.objects.filter(student=student_payment).exists():
+            raise ValidationError(
+                {"error": "Already exists a payment for this student"}
+            )
+        return serializer.save(student=student_payment)
