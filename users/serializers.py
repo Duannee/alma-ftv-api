fcf86cd508e70ee1299bf210253ae00167f37cb9
Duanne Moraes <duanne.souza@gmail.com>
2024-11-26 14:05:31 -0300

    feat: Create user serializer

---
+++ users/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "groups",
+            "user_permissions",
+        ]
+        extra_kwargs = {
+            "groups": {"required": False},
+            "user_permissions": {"required": False},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
