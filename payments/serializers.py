e6d1f9abd50dc09b0b3dfc104ce91efde87a4fb4
Duanne Moraes <duanne.souza@gmail.com>
2024-11-26 14:21:49 -0300

    feat: Create payment serializer

---
+++ payments/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = ["id", "student", "pay_day", "value", "status"]
+        depth = 1
+        read_only_fields = ["student"]
+
+    def validate(self, data):
+        for key in data.keys():
+            if key not in self.fields:
+                raise serializers.ValidationError({key: "This field does not exist."})
+        return data
