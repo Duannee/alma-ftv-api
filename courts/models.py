e0ae6bd12e198fb098a8741cc756d709d76626c7
Duanne Moraes <duanne.souza@gmail.com>
2024-12-04 14:32:22 -0300

    refactor: Put created at and updated at in courts model

--- courts/models.py
+++ courts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Court(models.Model):
     number = models.IntegerField()
     status = models.CharField(max_length=30)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
