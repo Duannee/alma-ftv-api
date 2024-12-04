c161232c841751df01b51330b866350b1ae7d6ad
Duanne Moraes <duanne.souza@gmail.com>
2024-12-04 14:57:43 -0300

    refactor: Create status choice and def str

--- courts/models.py
+++ courts/models.py
@@ -3,7 +3,11 @@ from django.utils.timezone import now
 
 
 class Court(models.Model):
+    STATUS_CHOICE = [("ACTIVE", "ACTIVE"), ("INACTIVE", "INACTIVE")]
     number = models.IntegerField()
-    status = models.CharField(max_length=30)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICE)
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.number} - {self.status} "
