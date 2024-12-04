c8deb33892406e25d0011200c6f7797b139b5e3f
Duanne Moraes <duanne.souza@gmail.com>
2024-12-04 14:58:56 -0300

    refactor: Create class time choice, created at and updated at

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class List(models.Model):
@@ -7,8 +8,25 @@ class List(models.Model):
         ("INTERMEDIARY", "INTERMEDIARY"),
         ("ADVANCED", "ADVANCED"),
     ]
+    CLASS_TIME_CHOICES = [
+        ("06:00", "06:00"),
+        ("07:00", "07:00"),
+        ("08:00", "08:00"),
+        ("09:00", "09:00"),
+        ("17:00", "17:00"),
+        ("18:00", "18:00"),
+        ("06:15", "06:15"),
+        ("07:15", "07:15"),
+        ("08:15", "08:15"),
+    ]
     date_day = models.DateField(auto_now_add=True)
-    student_id = models.ForeignKey(
-        "students.student", on_delete=models.CASCADE, related_name="list"
+    student = models.ForeignKey(
+        "students.student", on_delete=models.CASCADE, related_name="lists"
     )
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICE)
+    class_time = models.CharField(max_length=5, choices=CLASS_TIME_CHOICES)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.date_day} - {self.class_time}"
