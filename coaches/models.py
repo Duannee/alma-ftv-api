81e8af65d824de9e9303df4f8babc998ddf63145
Duanne Moraes <duanne.souza@gmail.com>
2024-12-04 15:08:48 -0300

    refactor: Change phone type, put created at and updated

--- coaches/models.py
+++ coaches/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Coach(models.Model):
     GENRE_CHOICE = [("FEMALE", "FEMALE"), ("MALE", "MALE"), ("OTHERS", "OTHERS")]
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.TextField(max_length=20)
+    phone = models.CharField(max_length=14)
     genre = models.CharField(max_length=7, choices=GENRE_CHOICE)
     profile_img = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.nome
