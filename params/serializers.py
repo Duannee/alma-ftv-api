92da96bb3d3ec2eff721a64941a92da369ea410d
Duanne Moraes <duanne.souza@gmail.com>
2024-12-05 15:53:41 -0300

    feat: Create params serializers

---
+++ params/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Params
+
+
+class ParamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Params
+        fields = [
+            "id",
+            "class_date",
+            "status",
+            "expires_at",
+            "description",
+            "category",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
