856ac0f22ae08423ab0d435751c2ffc73f5b1ff8
Duanne Moraes <duanne.souza@gmail.com>
2024-12-05 15:11:00 -0300

    feat: Create list courts model

--- list_courts/models.py
+++ list_courts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from courts.models import Court
+from lists.models import List
+from django.utils.timezone import now
 
-# Create your models here.
+
+class ListCourt(models.Model):
+    court = models.ManyToManyField(Court, related_name="list_courts")
+    lists = models.ManyToManyField(List, related_name="list_courts")
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
