94e12dbc162b6da8c2595d3ccc9ef1d0f1b2788f
Duanne Moraes <duanne.souza@gmail.com>
2024-12-05 15:37:21 -0300

    feat: Create params app

---
+++ params/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class Params(models.Model):
+    CATEGORY_CHOICE = [
+        ("BEGINNER", "BEGINNER"),
+        ("INTERMEDIARY", "INTERMEDIARY"),
+        ("ADVANCED", "ADVANCED"),
+    ]
+    class_date = models.DateField()
+    status = models.BooleanField(default=True)
+    expires_at = models.DateTimeField()
+    description = models.TextField()
+    category = models.CharField(max_length=12, choices=CATEGORY_CHOICE)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
