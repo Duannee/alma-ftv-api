8e73c9359f7b1a8b43f6e83e7a7814d17df3fb2f
Duanne Moraes <duanne.souza@gmail.com>
2024-12-09 14:30:52 -0300

    feat: Create retrieve update destroy route

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import StudentListCreateView
+from .views import StudentListCreateView, StudentRetrieveUpdateDestroyView
 
 urlpatterns = [
     path(
         "create/students/", StudentListCreateView.as_view(), name="list-create-students"
+    ),
+    path(
+        "update/students/<int:pk>/",
+        StudentRetrieveUpdateDestroyView.as_view(),
+        name="retrieve-update-destroy-students",
     ),
 ]
