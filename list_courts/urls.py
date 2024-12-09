5e1e6bdd71531cb6620933bcc866cb3a59338c60
Duanne Moraes <duanne.souza@gmail.com>
2024-12-09 14:48:55 -0300

    feat: Create lists courts url

---
+++ list_courts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import ListCourtsListCreateView, ListCourtsRetrieveUpdateDestroyView
+
+urls_patterns = [
+    path(
+        "create/lists/courts/",
+        ListCourtsListCreateView.as_view(),
+        name="list-create-list-courts",
+    ),
+    path(
+        "update/list/courts/<int:pk>",
+        ListCourtsRetrieveUpdateDestroyView.as_view(),
+        name="retrieve-update-destroy-courts",
+    ),
+]
