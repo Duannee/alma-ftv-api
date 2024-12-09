159781de4f35975ba96b48f43b8fbc50661afe70
Duanne Moraes <duanne.souza@gmail.com>
2024-12-09 14:55:13 -0300

    refactor: Put slash at the end of route

--- lists/urls.py
+++ lists/urls.py
@@ -4,7 +4,7 @@ from .views import ListCreateAPIView, ListRetrieveUpdateDestroyView
 urls_patterns = [
     path("create/lists/", ListCreateAPIView.as_view(), name="list-create-list"),
     path(
-        "update/lists/<int:pk>",
+        "update/lists/<int:pk>/",
         ListRetrieveUpdateDestroyView.as_view(),
         name="retrieve-update-destroy-list",
     ),
