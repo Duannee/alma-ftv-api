4bccd43e7842a9159934d0e065e529fdf83af2aa
Duanne Moraes <duanne.souza@gmail.com>
2024-12-09 14:20:00 -0300

    feat: Create list courts views

--- list_courts/views.py
+++ list_courts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import ListCourt
+from .serializers import ListCourtSerializer
 
-# Create your views here.
+
+class ListCourtsListCreateView(ListCreateAPIView):
+    queryset = ListCourt.objects.all()
+    serializer_class = ListCourtSerializer
+
+
+class ListCourtsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = ListCourt.objects.all()
+    serializer_class = ListCourtSerializer
