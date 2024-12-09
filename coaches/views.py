d11352523faeb3691da70be8335ef9c464d95d79
Duanne Moraes <duanne.souza@gmail.com>
2024-12-09 14:14:03 -0300

    feat: Create coach list create and coach retrieve update destroy views

--- coaches/views.py
+++ coaches/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Coach
 from .serializers import CoachSerializer
 
 
-class CreateCoachView(CreateAPIView):
+class CoachListCreateView(ListCreateAPIView):
+    queryset = Coach.objects.all()
+    serializer_class = CoachSerializer
+
+
+class CoachRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Coach.objects.all()
     serializer_class = CoachSerializer
