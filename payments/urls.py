3750dbf35f8b2a4888d42752f3cc53a96be706a0
Duanne Moraes <duanne.souza@gmail.com>
2024-12-09 14:33:56 -0300

    feat: Create list and retrieve update destroy routes

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import CreatePaymentView
+from .views import PaymentCreateView, PaymentListView, PaymentRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path("create/payment/", CreatePaymentView.as_view(), name="create-payment")
+    path("create/payment/", PaymentCreateView.as_view(), name="create-payment"),
+    path("list/payment/", PaymentListView.as_view(), name="list-payment"),
+    path(
+        "update/payment/<int:pk>/",
+        PaymentRetrieveUpdateDestroyView.as_view(),
+        name="retrieve-update-destroy-payment",
+    ),
 ]
