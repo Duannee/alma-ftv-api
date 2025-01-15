f7d47f7cdfbd327ebac6b047c696a7666dd9ccc7
Duanne Moraes <duanne.souza@gmail.com>
2025-01-15 16:12:46 -0300

    feat: Create the approved admission view

--- admissions/views.py
+++ admissions/views.py
@@ -2,9 +2,23 @@ from rest_framework.generics import ListAPIView, UpdateAPIView, DestroyAPIView
 from users.models import User
 from .serializers import AdmissionSerializer
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class AdmissionListView(ListAPIView):
     queryset = User.objects.filter(is_student=False)
     serializer_class = AdmissionSerializer
     permission_classes = [IsAdminUser]
+
+
+class ApproveAdmissionView(UpdateAPIView):
+    queryset = User.objects.filter(is_student=False)
+    serializer_class = AdmissionSerializer
+    permission_classes = [IsAdminUser]
+
+    def update(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.is_student = True
+        user.save()
+        return Response({"message": "Approved user!"}, status=status.HTTP_200_OK)
