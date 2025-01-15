6da5ec00364d0b2a8ab61e7698a52242937da2bf
Duanne Moraes <duanne.souza@gmail.com>
2025-01-15 15:26:47 -0300

    refactor: Change admission model name

--- admissions/models.py
+++ admissions/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class AdmissionModel(models.Model):
+class Admission(models.Model):
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="admissions"
     )
