2b26fd45f673f2a6ad13dfbdea2c30f4f41a005c
Duanne Moraes <duanne.souza@gmail.com>
2025-01-15 16:21:31 -0300

    refactor: Change admission colums in admission model

--- admissions/models.py
+++ admissions/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Admission(models.Model):
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         "users.User", on_delete=models.CASCADE, related_name="admissions"
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Admission Request - {self.user.username}"
