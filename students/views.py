8284a01f0033f18b5b58d821a335c0db5d06315f
Duanne Moraes <duanne.souza@gmail.com>
2025-01-16 18:17:47 -0300

    refactor: Add authentication in student views

--- students/views.py
+++ students/views.py
@@ -2,16 +2,25 @@ from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Student
 from .serializers import StudentSerializer
 
 
 class StudentListCreateView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+    def get_queryset(self):
+        return Student.objects.filter(user=self.request.user)
+
 
 class StudentRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
