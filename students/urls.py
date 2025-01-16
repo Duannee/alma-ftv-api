ddba12ab5eac95de0b2028c9e873473ca0f951c8
Duanne Moraes <duanne.souza@gmail.com>
2025-01-16 18:15:56 -0300

    feat: Create the get me student route

--- students/urls.py
+++ students/urls.py
@@ -6,11 +6,16 @@ urlpatterns = [
     path(
         "create/students/",
         StudentListCreateView.as_view(),
-        name="list-create-students",
+        name="create-students",
     ),
     path(
         "update/students/<int:pk>/",
         StudentRetrieveUpdateDestroyView.as_view(),
         name="retrieve-update-destroy-students",
+    ),
+    path(
+        "getme/student/",
+        StudentListCreateView.as_view(),
+        name="getme-students",
     ),
 ]
