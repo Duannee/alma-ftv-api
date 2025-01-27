9368853d4c749b61b8732491c3ca59e414d327ae
Duanne Moraes <duanne.souza@gmail.com>
2025-01-27 14:15:21 -0300

    refactor: Put frequency of classes in student model

--- students/models.py
+++ students/models.py
@@ -13,6 +13,14 @@ class Student(models.Model):
         ("MALE", "MALE"),
         ("OTHERS", "OTHERS"),
     ]
+    FREQUENCY_OF_CLASSES_CHOICES = [
+        ("1X", "1X"),
+        ("2X", "2X"),
+        ("3X", "3X"),
+        ("4X", "4X"),
+        ("5X", "5X"),
+        ("6X", "6X"),
+    ]
 
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="students"
@@ -22,6 +30,9 @@ class Student(models.Model):
     genre = models.CharField(max_length=7, choices=GENRE_CHOICE)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICE)
     profile_img = models.TextField(null=True, blank=True)
+    frequency_of_classes = models.CharField(
+        max_length=2, choices=FREQUENCY_OF_CLASSES_CHOICES
+    )
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
