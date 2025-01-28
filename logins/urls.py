3ccebcd98a4eee390f4c777aa2ef2fd7760cc048
Duanne Moraes <duanne.souza@gmail.com>
2025-01-28 15:16:25 -0300

    refactor: Change the view in login url, put the new custom view

--- logins/urls.py
+++ logins/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import LoginView
+from .views import CustomLoginView
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="login"),
+    path("login/", CustomLoginView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
 ]
