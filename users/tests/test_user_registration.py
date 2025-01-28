f1e7fe33ecdb0a98ce4b12cb4c628534b61fe92b
Duanne Moraes <duanne.souza@gmail.com>
2025-01-28 17:38:30 -0300

    feat: Create test to verify user creation fails when password is not provided

--- users/tests/test_user_registration.py
+++ users/tests/test_user_registration.py
@@ -39,3 +39,16 @@ class UserRegistrationTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("email", response.data)
         self.assertFalse(User.objects.filter(first_name=payload["first_name"]).exists())
+
+    def test_create_user_without_password_fails(self):
+        """Tests whether user creation fails when password is not provided"""
+        payload = {
+            "first_name": "test",
+            "last_name": "tests",
+            "email": "test@example.com",
+            "is_student": False,
+        }
+        response = self.client.post(self.register_url, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("password", response.data)
+        self.assertFalse(User.objects.filter(email=payload["email"]).exists())
