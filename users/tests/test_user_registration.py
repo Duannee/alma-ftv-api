b0fd63bb450a8f962673100c6b8b791ef33cf4cd
Duanne Moraes <duanne.souza@gmail.com>
2025-01-28 17:39:39 -0300

    feat: Create test to verify user creation fails when trying to use an existing email

--- users/tests/test_user_registration.py
+++ users/tests/test_user_registration.py
@@ -52,3 +52,11 @@ class UserRegistrationTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("password", response.data)
         self.assertFalse(User.objects.filter(email=payload["email"]).exists())
+
+    def test_create_user_with_existing_email_fails(self):
+        """Tests whether user creation fails when trying to use an existing email"""
+        User.objects.create_user(email="test@example.com", password="testpassword123")
+        response = self.client.post(self.register_url, self.valid_payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
