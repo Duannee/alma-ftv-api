518cbc020c8a3fd23884a038b447f6e3a884ffaa
Duanne Moraes <duanne.souza@gmail.com>
2025-01-28 17:31:43 -0300

    refactor: Put groups and user permissions in def create and update on user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -24,18 +24,26 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
+        groups = validated_data.pop("groups", [])
+        user_permissions = validated_data.pop("user_permissions", [])
         password = validated_data.pop("password", None)
         user = User(**validated_data)
         if password:
             user.set_password(password)
         user.save()
+        user.groups.set(groups)
+        user.user_permissions.set(user_permissions)
         return user
 
     def update(self, instance, validated_data):
+        groups = validated_data.pop("groups", [])
+        user_permissions = validated_data.pop("user_permissions", [])
         password = validated_data.pop("password", None)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         if password:
             instance.set_password(password)
         instance.save()
+        instance.groups.set(groups)
+        instance.user_permissions.set(user_permissions)
         return instance
