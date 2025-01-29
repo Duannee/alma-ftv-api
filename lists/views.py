1bcbeb2af5e850a3f32a6e760d71f74567866444
Duanne Moraes <duanne.souza@gmail.com>
2025-01-29 17:41:49 -0300

    feat: Create student available for courts view and put query set in list by time view

--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,13 @@
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
+    ListAPIView,
 )
 
 from .models import List
 from .serializers import ListSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from list_courts.models import ListCourt
 
 
 class ListsListCreateView(ListCreateAPIView):
@@ -22,4 +24,36 @@ class ListByTimeView(ListCreateAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
     filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["class_time", "list_params__category"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        user = self.request.user
+        if user.is_authenticated:
+            queryset = queryset.filter(list_params__category=user.category)
+
+        return queryset
+
+
+class StudentsAvailableForCourtsView(ListAPIView):
+    serializer_class = ListSerializer
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ["class_time"]
+
+    def get_queryset(self):
+        class_time = self.request.query_params.get("class_time")
+
+        user = self.request.user
+        if not user.is_authenticated:
+            return List.objects.none()
+
+        student_category = user.category
+
+        students_in_list = List.objects.filter(
+            class_time=class_time, student__category=student_category
+        )
+
+        students_in_court = ListCourt.objects.values_list("list_id", flat=True)
+
+        return students_in_list.exclude(id__in=students_in_court)
