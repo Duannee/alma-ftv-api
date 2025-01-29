60ab8a81f379388d5e23e668275a853f0cb4d162
Duanne Moraes <duanne.souza@gmail.com>
2025-01-29 14:52:00 -0300

    refactor: Create get query set in list create view for use django filter

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,24 @@ from rest_framework.generics import (
 
 from .models import List
 from .serializers import ListSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ListsListCreateView(ListCreateAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["class_time"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        class_time = self.request.query_params.get("class_time")
+
+        if class_time:
+            queryset = queryset.filter(class_time=class_time)
+
+        return queryset
 
 
 class ListsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
