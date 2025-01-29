afb6a965751cab261398d6509f0586029dfed6f9
Duanne Moraes <duanne.souza@gmail.com>
2025-01-29 15:45:35 -0300

    feat: Create view list by time

--- lists/views.py
+++ lists/views.py
@@ -11,20 +11,15 @@ from django_filters.rest_framework import DjangoFilterBackend
 class ListsListCreateView(ListCreateAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["class_time"]
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-        class_time = self.request.query_params.get("class_time")
-
-        if class_time:
-            queryset = queryset.filter(class_time=class_time)
-
-        return queryset
 
 
 class ListsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+
+
+class ListByTimeView(ListCreateAPIView):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["class_time"]
