83d2c60edde1b68b406981fee872f6c93bd2ba17
Duanne Moraes <duanne.souza@gmail.com>
2025-01-29 17:46:03 -0300

    refactor: Change the relationship from court and lists in List court model

--- list_courts/models.py
+++ list_courts/models.py
@@ -6,7 +6,16 @@ from lists.models import List
 
 
 class ListCourt(models.Model):
-    court = models.ManyToManyField(Court, related_name="list_courts")
-    lists = models.ManyToManyField(List, related_name="list_courts")
+    court = models.ForeignKey(
+        Court, on_delete=models.CASCADE, related_name="list_courts"
+    )
+    lists = models.OneToOneField(
+        List, on_delete=models.CASCADE, related_name="list_courts"
+    )
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (
+            f"{self.list.student.name} - Court {self.court.id} - {self.list.class_time}"
+        )
