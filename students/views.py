01e9b670f5ddf0d7cdebe92d6757b30f8300f126
Duanne Moraes <duanne.souza@gmail.com>
2025-02-11 16:06:58 -0300

    refactor: Change the logic in get me student view

--- students/views.py
+++ students/views.py
@@ -7,7 +7,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Student
-from .serializers import StudentSerializer
+from .serializers import StudentSerializer, UserStudentSerializer
+
+from rest_framework.response import Response
 
 
 class StudentListCreateView(ListCreateAPIView):
@@ -30,10 +32,25 @@ class StudentRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 class GetMeStudentView(ListAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
-    serializer_class = StudentSerializer
 
     def get_queryset(self):
         user = self.request.user
         if user.is_superuser:
-            return Student.objects.all()
-        return Student.objects.filter(user=user)
+            return [user]
+
+        if user.is_student:
+            student = Student.objects.filter(user=user).first()
+            if student:
+                return [student]
+
+        return []
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        if self.request.user.is_superuser:
+            serializer = UserStudentSerializer(queryset, many=True)
+        else:
+            serializer = StudentSerializer(queryset, many=True)
+
+        return Response(serializer.data, status=200)
