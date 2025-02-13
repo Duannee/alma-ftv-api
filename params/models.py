9ea9def06ffc66914ba9aca468137cc42527483d
Duanne Moraes <duanne.souza@gmail.com>
2025-02-13 16:30:55 -0300

    feat: Create unit choices and unit table in ListParams model

--- params/models.py
+++ params/models.py
@@ -10,11 +10,18 @@ class ListParams(models.Model):
         ("WARNING", "WARNING"),
         ("WEEKEND", "WEEKEND"),
     ]
+
+    UNIT_CHOICES = [
+        ("IPANEMA", "IPANEMA"),
+        ("BARRA", "BARRA"),
+    ]
+
     class_date = models.DateField()
     status = models.BooleanField(default=True)
     expires_at = models.DateTimeField()
     description = models.TextField()
     category = models.CharField(max_length=12, choices=CATEGORY_CHOICE)
+    unit = models.CharField(max_length=7, choices=UNIT_CHOICES)
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
