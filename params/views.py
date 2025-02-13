5cd335505e7b51854a060a723614a4413ee94eac
Duanne Moraes <duanne.souza@gmail.com>
2025-02-13 15:04:26 -0300

    refactor: Change Params list create view for Params create view

--- params/views.py
+++ params/views.py
@@ -1,13 +1,15 @@
 from rest_framework.generics import (
-    ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
+    CreateAPIView,
+    ListAPIView,
 )
+
 
 from .models import ListParams
 from .serializers import ListParamSerializer
 
 
-class ParamsListCreateView(ListCreateAPIView):
+class ParamsCreateView(CreateAPIView):
     queryset = ListParams.objects.all()
     serializer_class = ListParamSerializer
 
