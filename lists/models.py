fb524c267cac060299c7e46e02095a285f32ea70
Duanne Moraes <duanne.souza@gmail.com>
2025-02-13 16:58:57 -0300

    fix: Remove class time from List model

--- lists/models.py
+++ lists/models.py
@@ -4,24 +4,12 @@ from django.utils.timezone import localtime, now
 
 
 class List(models.Model):
-    CLASS_TIME_CHOICES = [
-        ("06:00", "06:00"),
-        ("07:00", "07:00"),
-        ("08:00", "08:00"),
-        ("09:00", "09:00"),
-        ("17:00", "17:00"),
-        ("18:00", "18:00"),
-        ("06:15", "06:15"),
-        ("07:15", "07:15"),
-        ("08:15", "08:15"),
-    ]
     student = models.ForeignKey(
         "students.Student", on_delete=models.CASCADE, related_name="lists"
     )
     list_params = models.ForeignKey(
         "params.ListParams", on_delete=models.CASCADE, related_name="lists"
     )
-    class_time = models.CharField(max_length=5, choices=CLASS_TIME_CHOICES)
     created_at = models.DateTimeField(default=now, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
