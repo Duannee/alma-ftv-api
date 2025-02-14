3d6d98315c50c17178251e5304320b5c404eec9e
Duanne Moraes <duanne.souza@gmail.com>
2025-02-14 11:37:55 -0300

    refactor: Put authentication_class and permission_class in UserUpdateView

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import CreateAPIView, UpdateAPIView
 
 from .models import User
 from .serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserRegisterView(CreateAPIView):
@@ -10,6 +12,7 @@ class UserRegisterView(CreateAPIView):
 
 
 class UserUpdateView(UpdateAPIView):
-
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
