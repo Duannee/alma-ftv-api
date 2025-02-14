3c16b48178ae68cfe8b2cc81311065eac907e388
Duanne Moraes <duanne.souza@gmail.com>
2025-02-14 11:36:31 -0300

    feat: Create UserUpdateView in users model

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 
 from .models import User
 from .serializers import UserSerializer
 
 
 class UserRegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateView(UpdateAPIView):
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
