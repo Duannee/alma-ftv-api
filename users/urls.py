7e64390d3870b9f3ef9ac07bee9082a349b8c57c
Duanne Moraes <duanne.souza@gmail.com>
2025-02-14 11:37:00 -0300

    feat: Create user update url in users urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserRegisterView
+from .views import UserRegisterView, UserUpdateView
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path("user/update/<int:pk>/", UserUpdateView.as_view(), name="update"),
 ]
