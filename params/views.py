293253f8ac34307676b236af6759c8fc8a5ee8fa
Duanne Moraes <duanne.souza@gmail.com>
2025-02-17 16:41:31 -0300

    feat: Create AvailableClassTimeParamsView in params view

--- params/views.py
+++ params/views.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
     CreateAPIView,
@@ -7,6 +8,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.utils.timezone import now
+from rest_framework.response import Response
 
 
 from .models import ListParams
@@ -28,3 +31,31 @@ class ParamsListView(ListAPIView):
 class ParamsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = ListParams.objects.all()
     serializer_class = ListParamSerializer
+
+
+class AvailableClassTimeParamsView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        today = now().date()
+        tomorrow = now().date() + timedelta(days=1)
+
+        user_category = user.category
+        user_unit = user.unit
+
+        weekday = tomorrow.weekday()
+        valid_times = (
+            ListParams.CATEGORY_TIMES_BY_UNIT.get(user_category, {})
+            .get(user_unit, {})
+            .get(weekday, [])
+        )
+
+        return valid_times
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        formatted_times = [time.strftime("%H:%M") for time in queryset]
+
+        return Response({"available_times": formatted_times})
