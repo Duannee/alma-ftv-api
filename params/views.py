c6f3bd532a243e9c61b7405c82fcb25b8dec8184
Duanne Moraes <duanne.souza@gmail.com>
2025-02-18 17:44:58 -0300

    fix: Delete AvailableClassTimeParamsView and SelectClassTimeParamsView from params view

--- params/views.py
+++ params/views.py
@@ -7,10 +7,6 @@ from rest_framework.generics import (
 )
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
-from django.utils.timezone import now
-from rest_framework.response import Response
 
 
 from .models import ListParams
@@ -32,44 +28,3 @@ class ParamsListView(ListAPIView):
 class ParamsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = ListParams.objects.all()
     serializer_class = ListParamSerializer
-
-
-class AvailableClassTimeParamsView(ListAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        today = now().date()
-        tomorrow = now().date() + timedelta(days=1)
-
-        user_category = user.category
-        user_unit = user.unit
-
-        weekday = tomorrow.weekday()
-        valid_times = (
-            ListParams.CATEGORY_TIMES_BY_UNIT.get(user_category, {})
-            .get(user_unit, {})
-            .get(weekday, [])
-        )
-
-        return valid_times
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        formatted_times = [time.strftime("%H:%M") for time in queryset]
-
-        return Response({"available_times": formatted_times})
-
-
-class SelectClassTimeParamsView(UpdateAPIView):
-    queryset = ListParams.objects.all()
-    serializer_class = ListParamSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.class_time = request.data.get("class_time")
-        instance.save()
-        return Response({"message": "Class time updated successfully"})
